14b63ed18b1965f143ae7b753e57b4f2758c34c9
Anderson Freitas <108918987+freitasanderson@users.noreply.github.com>
2022-11-20 19:38:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export function Head(){
         integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous" defer>
       </script>
 
-      <link rel="stylesheet" href="/src/assets/CSS/style.css"/>
+      <link rel="stylesheet" href="/assets/CSS/style.css"/>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" defer/>
 
